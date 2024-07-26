8884fdba548b2671a2b24542846aa96a4d0dbb4c
Mutlu Kurt <81134748+mutlukurt@users.noreply.github.com>
2024-07-26 22:24:02 +0300

    int() Function Example1
    Python int() with different DataTypes

---
+++ # int() on string representation of numb.py
@@ -0,0 +1,8 @@
+# int() on string representation of numbers
+print("int('9')) =", int('9'))
+
+# int() on float values
+print("int(9.9) =", int(9.9))
+
+# int() on Python integer
+print("int(9) =", int(9))
